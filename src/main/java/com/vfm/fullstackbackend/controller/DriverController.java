58ee6a6fe8a65d804a2ca9f34bba152d75576789
unknown <webdesign1954@hotmail.com>
2023-01-25 15:52:33 +0100

    DriverController Post and Get

---
+++ src/main/java/com/vfm/fullstackbackend/controller/DriverController.java
@@ -0,0 +1,30 @@
+package com.vfm.fullstackbackend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.vfm.fullstackbackend.model.Driver;
+import com.vfm.fullstackbackend.repository.DriverRepository;
+
+
+@RestController
+public class DriverController {
+
+	@Autowired
+	private DriverRepository driverRepository;
+
+	@PostMapping("/driver")
+	Driver newDriver(@RequestBody Driver newDriver) {
+		return driverRepository.save(newDriver);
+	}
+
+	@GetMapping("/drivers")
+	List<Driver> getDrivers() {
+		return driverRepository.findAll();
+	}
+
+}
