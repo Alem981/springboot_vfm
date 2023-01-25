b6c7cab535680ef62b084b1fd1c4118caa63c530
unknown <webdesign1954@hotmail.com>
2023-01-25 20:42:47 +0100

    exceptions

--- src/main/java/com/vfm/fullstackbackend/controller/DriverController.java
+++ src/main/java/com/vfm/fullstackbackend/controller/DriverController.java
@@ -3,6 +3,7 @@ package com.vfm.fullstackbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.vfm.fullstackbackend.repository.DriverRepository;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class DriverController {
 
 	@Autowired
---
+++ src/main/java/com/vfm/fullstackbackend/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.vfm.fullstackbackend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vfm.fullstackbackend.exception.UserNotFoundException;
+import com.vfm.fullstackbackend.model.User;
+ import com.vfm.fullstackbackend.repository.UserRepository;
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping("/user")
+	User newUser(@RequestBody User newUser) {
+		return userRepository.save(newUser);
+	}
+
+	@GetMapping("/users")
+	List<User> getUsers() {
+		return userRepository.findAll();
+	}
+	@GetMapping("/user/{id}")
+	User getUserById(@PathVariable Long id) {
+		return userRepository.findById(id)
+				.orElseThrow(()->new UserNotFoundException(id));
+	}
+
+}
